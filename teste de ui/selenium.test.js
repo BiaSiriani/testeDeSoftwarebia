b2c4ba5263db42a9e081ff805213a7d7b95bb69d
biasiriani <biasiriani200722@gmail.com>
2026-02-19 08:17:11 -0300

    vai

--- teste de ui/selenium.test.js
+++ teste de ui/selenium.test.js
@@ -3,13 +3,13 @@ const { Builder, Browser, By, Key, until } = require('selenium-webdriver')
 const iniciarTeste = async () => {
     let driver = await new Builder().forBrowser(Browser.CHROME).build()
 
-    await driver.get('https://google.com');
-
-    const input = await driver.findElement(By.css("textarea"));
-    await input.sendKeys("goku");
-await input.sendKeys(Key.RETURN);
-
-
+    await driver.get("https://http.cat" );
+    const urls = await driver.findElements(By.css('li a'));
+    console.log (urls.length);
+     
+    for(var elementos of urls){
+        console.log(elementos)
+    }
 await driver.close();
 }
 
